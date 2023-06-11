ac219ccede1ae4d443858e04f4f8aef5ae53a652
muzammil2208 <muzammilsk90@gmail.com>
2023-06-11 20:44:27 +0530

    basic ui layout complete

--- pages/index.js
+++ pages/index.js
@@ -14,10 +14,13 @@ export default function Home() {
   else
   {
     return(
-      <div className="bg-blue-900 w-screen h-screen "> 
-        <Nav/>
-      you are logged in as {session.user.email}
-      <button onClick={()=>signOut()} className="rounded-lg bg-white p-2 px-4 text-black">sign out</button></div>
+      <div className="bg-blue-900 w-screen h-screen flex"> 
+      <Nav/>
+      <div className=" flex flex-grow bg-white p-4 gap-2 mt-2 mr-2 mb-2 rounded-lg">
+          you are logged in as {session.user.email}
+           <button onClick={()=>signOut()} className="rounded-lg w-[100px] h-[50px] bg-red-200 p-2 px-4 text-black">sign out</button></div>
+
+      </div>
       
     )
   }

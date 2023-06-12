0408619fdbea77f74ea19a7150fa0f74ec5f7504
muzammil2208 <muzammilsk90@gmail.com>
2023-06-12 12:51:06 +0530

    products functionality added

--- pages/Products.js
+++ pages/Products.js
@@ -1,9 +1,24 @@
+import Link from 'next/link'
 import Layout from '../components/Layout'
 export default function Products()
 {
     return(
         <Layout>
-            This is products page
+        <div className="grid grid-cols-2 gap-3 w-[100%]">
+        <Link href={"/Products/new"} className=" w-45 h-45 py-1 px-2 flex justify-center items-center bg-gray-300 rounded-lg">
+                 Add Product
+            </Link>
+            <Link href={"/Products/update"} className=" w-50 py-1 px-2 h-10 flex justify-center items-center bg-gray-300 rounded-lg">
+                 Update Product
+            </Link>
+            <Link href={"/Products/delete"} className=" w-50 py-1 px-2 h-10 flex justify-center items-center bg-gray-300 rounded-lg">
+                 Delete Product
+            </Link>
+            <Link href={"/Products/view"} className=" w-50 py-1 px-2 h-10 flex justify-center items-center bg-gray-300 rounded-lg">
+                 View Products
+            </Link>
+        </div>
+         
         </Layout>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home()
             <div className="flex gap-2 h-10 rounded-lg p-1 overflow-hidden">
               <img src={session?.user.image} className="w-8 h-8 rounded-xl"/>
               <span>{session?.user.name}</span>
-              <button onClick={()=>signOut()} className="rounded-lg bg-red-500"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <button onClick={()=>signOut()} className=" rounded-2xl h-8 w-8 flex justify-center items-center bg-red-500"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
 </svg>
               </button>

20df6b648d83199f6849d43902ef5ea102234cf7
muzammil2208 <muzammilsk90@gmail.com>
2023-06-15 11:33:12 +0530

    added api for products page

--- components/Nav.js
+++ components/Nav.js
@@ -19,27 +19,27 @@ export default function Nav()
                     </span>
                 </Link>
                 <nav className="flex flex-col gap-2">
-                <Link href={'/'} className={pathname === "/" ? activeLink : inactiveLink}>
+                <Link href={'/'} className={pathname==="/" ? activeLink : inactiveLink}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
                         </svg>
                     <span>Dashboard</span>
                 </Link>
-                <Link href={'/Orders'} className={pathname === "/Orders" ? activeLink : inactiveLink}>
+                <Link href={'/Orders'} className={pathname.startsWith("/Orders")? activeLink : inactiveLink}>
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
                             </svg>
 
                     <span>Orders</span>
                 </Link>
-                <Link href={'/Products'} className={pathname === "/Products" ? activeLink : inactiveLink}>
+                <Link href={'/Products'} className={pathname.startsWith("/Products") ? activeLink : inactiveLink}>
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" />
                         </svg>
 
                     <span>Products</span>
                 </Link>
-                <Link href={'/Settings'} className={pathname === "/Settings" ? activeLink : inactiveLink}>
+                <Link href={'/Settings'} className={pathname.startsWith("/Settings") ? activeLink : inactiveLink}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 011.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.56.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.893.149c-.425.07-.765.383-.93.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 01-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.397.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 01-.12-1.45l.527-.737c.25-.35.273-.806.108-1.204-.165-.397-.505-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.107-1.204l-.527-.738a1.125 1.125 0 01.12-1.45l.773-.773a1.125 1.125 0 011.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894z" />
                     <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
--- pages/Products/New.js
+++ pages/Products/New.js
@@ -1,20 +1,29 @@
+import axios from "axios";
 import Layout from "../../components/Layout"
+import {useState} from 'react'
 export default function New()
 {
+    const [title,setTtitle]=useState('');
+    const [description,setDescription]=useState('')
+    const [price,setPrice]=useState('');
+    async function createProduct(ev)
+    {
+        ev.preventDefault();
+        const data={title,description,price};
+        await axios.post('/api/Products',data);
+    }
     return(
-    
     <Layout>
     <div className="flex flex-col w-[100%] gap-4 h-[100%]">
     <h2 className="text-blue-900 text-3xl flex w-[100%] justify-center"> Add Products</h2>
-        <form className="flex flex-col gap-4 justify-evenly w-[100%] h-[100%]">
+        <form className="flex flex-col gap-4 justify-evenly w-[100%] h-[100%]" onSubmit={createProduct}>
             <label>Name</label>
-            <input type="text" placeholder="Product Name"></input>
+            <input type="text" value={title} placeholder="Product Name" onChange={ev=>setTtitle(ev.target.value)}></input>
             <label>Description</label>
-            <textarea placeholder="product description">
-            </textarea>
+            <textarea value={description} onChange={ev=>setDescription(ev.target.value)} placeholder="product description"/>
             <label>Price(USD)</label>
-            <input type="number" placeholder="Product Name"></input>
-            <button className=" bg-gray-400 px-3 py-2 rounded-lg hover:bg-blue-900 text-white">Add</button>
+            <input value={price} onChange={ ev =>setPrice(ev.target.value)} type="number" placeholder="Product Name"></input>
+            <button  className=" bg-gray-400 px-3 py-2 rounded-lg hover:bg-blue-900 text-white" type="submit">Add</button>
         </form>
     </div>
         
